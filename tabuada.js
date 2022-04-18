52f220f4d6faf9a1c492f0a7ec0d9de89f942e57
Lennon <lennonsilva@dgtbr.com.br>
2022-04-18 11:54:24 -0400

    tabuada como função

--- tabuada.js
+++ tabuada.js
@@ -1,7 +1,19 @@
-let numero1 = 7;
+/*let numero1 = 7;
 let x;
 
 for (let x = 1; x <= 10; x++) {
     console.log(numero1 +  " x " + x + " = " + (numero1*x));
     
+}*/
+
+function tabuada(numero) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(i + " x " + numero  + " = " + (numero * i));
+    }
+    console.log("\n");
+    return;
+
 }
+tabuada (3);
+tabuada (5);
+tabuada (7);
