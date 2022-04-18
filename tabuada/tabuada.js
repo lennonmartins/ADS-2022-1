85c540b9ce35c10d0c694fa61e98de1228d7de67
Lennon <lennonsilva@dgtbr.com.br>
2022-04-18 13:47:27 -0400

    tabuada com NPM

--- tabuada/tabuada.js
+++ tabuada/tabuada.js
@@ -1,6 +1,5 @@
 /*let numero1 = 7;
 let x;
-
 for (let x = 1; x <= 10; x++) {
     console.log(numero1 +  " x " + x + " = " + (numero1*x));
     
@@ -17,3 +16,15 @@ function tabuada(numero) {
 tabuada (3);
 tabuada (5);
 tabuada (7);
+
+
+/*
+let teclado = require('prompt-sync')();
+
+console.log('Digite o valor a ser multiplicado: ');
+let entrada = teclado();
+
+for (let i = 1; i <= 10; i++) {
+console.log(i + " x " + entrada  + " = " + (entrada * i));
+}
+    console.log("\n"); */
